5bba0189aac13695c6602692e7ac98756160b4b4
Javier Arauz <e0929036747@live.uleam.edu.ec.com>
2021-08-30 20:00:21 -0500

    JSON

---
+++ JSON.js
@@ -0,0 +1,83 @@
+{
+    "DatosEstudiantes":[
+
+{
+    "Cedula":"131672839",
+    "Apellido":"Arauz Moran",
+    "Nombre":"Victor Daniel",
+    "Semestre":"segundo",
+    "Paralelo":"D"
+    
+},
+{
+    "Cedula":"1316342139",
+    "Apellido":"Barreto Vera",
+    "Nombre":"Luis Antonio",
+    "Semestre":"Tercero",
+    "Paralelo":"A"
+ 
+
+},
+{
+    "Cedula":"1317832139",
+    "Apellido":"Vinces Cedeño",
+    "Nombre":"Antonella Denisse",
+    "Semestre":"Cuarto",
+    "Paralelo":"B"
+   
+},
+{
+    "Cedula":"1387514839",
+    "Apellido":"Cedeño Cedeño",
+    "Nombre":"Daniela Gabriela",
+    "Semestre":"Tercero",
+    "Paralelo":"C"
+
+},
+{
+    "Cedula":"1316938629",
+    "Apellido":"Gonzales Loor",
+    "Nombre":"Kevin David",
+    "Semestre":"Quinto",
+    "Paralelo":"A"
+},
+{
+    "Cedula":"1309127839",
+    "Apellido":"Moreira Moran",
+    "Nombre":"Mario Jesus",
+    "Semestre":"Tercero",
+    "Paralelo":"C"
+},
+{
+    "Cedula":"1310029139",
+    "Apellido":"Quiñonez Marrin",
+    "Nombre":"Ariana Maria",
+    "Semestre":"Primer",
+    "Paralelo":"A"
+},
+{
+    "Cedula":"131672839",
+    "Apellido":"Avila Vera",
+    "Nombre":"David Daniel",
+    "Semestre":"Cuarto",
+    "Paralelo":"B"
+},
+{
+    "Cedula":"1318723839",
+    "Apellido":"Macias Zambrano",
+    "Nombre":"Ximena Yanina",
+    "Semestre":"Quinto",
+    "Paralelo":"C"
+},
+
+ {
+    "Cedula":"1303982234",
+    "Apellido":"Mendoza Moreira",
+    "Nombre":"Ana Gabriela",
+    "Semestre":"Primer",
+    "Paralelo":"B"
+  }
+
+]
+
+}
